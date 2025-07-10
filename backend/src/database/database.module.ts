682143d6f3bea584c10b71d6d44cc6e2025cc382
Dimitrije Vukovic <dmtr.develop@gmail.com>
2025-07-10 18:53:18 +0200

    feat(config): configure dotenv for database and JWT setup

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -1,17 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'istorija8',
-      database: 'oglasi',
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      synchronize: true,        //dev
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,  // dev
+      }),
     }),
   ],
 })
