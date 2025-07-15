be6fc35d6778b6e2c72a025e42c971091649fbec
dmtrhub <dmtr.develop@gmail.com>
2025-07-16 01:12:28 +0200

    fix: volumes for seed in docker-compose

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("ad46913e6be170d2")
+/******/ 		__webpack_require__.h = () => ("041a16c55a9e108c")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
